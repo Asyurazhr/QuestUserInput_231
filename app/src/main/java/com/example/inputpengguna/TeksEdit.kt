d79e1e9d97c5092ac6ac57f65d53a773765e3505
Asyurazhr <Asyuradjola2004@gmail.com>
2025-10-30 14:58:51 +0700

    Menambahkan garis pemisah horizontal berwarna abu tua dengan ketebalan dan jarak atas-bawah yang diambil dari resource dimensi.

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -103,3 +103,10 @@ fun FormDataDiri(modifier: Modifier
             Text(text = stringResource(id = R.string.submit))
         }
 
+        HorizontalDivider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
