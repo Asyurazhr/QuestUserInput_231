ce7b8ee363bbfdae4e65bcb14e3018b723962bd0
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-01 12:44:07 +0700

    Memperbaiki code HorizontalDivider

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -226,16 +226,15 @@ fun FormDataDiri(modifier: Modifier
                 }
             )
 
+            HorizontalDivider(
+                modifier = Modifier.padding(
+                    bottom = dimensionResource(id = R.dimen.padding_medium),
+                    top = dimensionResource(id = R.dimen.padding_medium)
+                ),
+                thickness = dimensionResource(id = R.dimen.divider_tipis),
+                color = Color.DarkGray
+            )
 
-        HorizontalDivider(
-            modifier = Modifier.padding(
-                bottom = dimensionResource(R.dimen.padding_medium),
-                top = dimensionResource(
-                id = R.dimen.padding_medium
-            )),
-            thickness = dimensionResource(R.dimen.divider_tipis),
-            color = Color.DarkGray
-        )
         Button(
             modifier = Modifier.fillMaxWidth(fraction = 1f),
             enabled = textAlamat.isNotEmpty(),
