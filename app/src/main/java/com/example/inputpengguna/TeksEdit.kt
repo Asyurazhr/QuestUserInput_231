04a58e78621fe02138fdd3a4b1f85645f567781b
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-01 12:06:51 +0700

    Menambahkan space

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -76,8 +77,9 @@ fun FormDataDiri(modifier: Modifier
                     modifier = Modifier.fillMaxWidth()
                 )
             }
-        }
 
+            Spacer(modifier = Modifier.height(20.dp))
+        }
     Column(modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally) {
