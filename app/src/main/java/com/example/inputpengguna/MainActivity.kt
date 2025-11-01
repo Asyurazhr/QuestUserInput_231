51c7180d1ee77e785b4f947ed83191048429d0e2
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-01 11:24:54 +0700

    Menambahkan test status

--- app/src/main/java/com/example/inputpengguna/MainActivity.kt
+++ app/src/main/java/com/example/inputpengguna/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.inputpengguna.FormDataDiri
 import com.example.inputpengguna.ui.theme.InputPenggunaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             InputPenggunaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InputPenggunaTheme {
-        Greeting("Android")
     }
 }
